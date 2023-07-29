46351a4f02faa5c3e80e537665a08d0ce98efb01
kinnerasasank <121881690+kinnerasasank@users.noreply.github.com>
2023-07-29 18:13:14 +0000

    rename

--- queries/01_03/ConditionalFunctions.sql
+++ queries/01_03/ConditionalFunctions.sql
@@ -3,5 +3,5 @@ CASE job WHEN 'MANAGER' THEN 1.20*salary
          WHEN 'ANALYST' THEN 1.15*salary
          WHEN 'CLERK' THEN 1.10*salary
          ELSE salary
-         END 'Revised Salary'
+         END 'Revised Salary' 
 FROM emp_tab;
